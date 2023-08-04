378b8c87f68df65d7d7d8b15104726d0def060fa
Marcus <mgalvaoboni@gmail.com>
2023-08-03 22:31:09 -0300

    new learning POO

--- src/index.ts
+++ src/index.ts
@@ -273,3 +273,115 @@ export const fruits: FruitCountProps = {
   banana: 'banana',
   limao: 'limao',
 };
+
+class Empresa {
+  public readonly nome: string;
+  protected readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string; // A diferença entre private e protected é que private é somente acessível na class pai(super)
+  // e protected permite as subclasses acessarem também
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('sim', '11.002.111/0001-11');
+const colaborador1 = new Colaborador('Marcus', 'Boni');
+// empresa1.adicionaColaborador({ nome: 'marcus', sobrenome: 'boni' });
+empresa1.adicionaColaborador(colaborador1);
+console.log(empresa1);
+console.log(colaborador1);
+
+export class Guy {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+    private readonly idade: number, //encapsulamento: não permite que a propriedade seja acessada fora da classe
+    private readonly cpf: number,
+  ) {}
+
+  getNome(): string {
+    return this.nome;
+  }
+  getIdade(): number {
+    return this.idade;
+  }
+
+  getNomeCompleto(): string {
+    return `${this.nome} ${this.sobrenome}`;
+  }
+}
+
+export class Aluno extends Guy {
+  getNomeCompleto() {
+    //Polimorfismo: Capacidade de sobrescrever o método da classe pai na classe filha
+    return `Aluno ${this.nome} ${this.sobrenome}`;
+  }
+}
+
+export class Cliente extends Guy {
+  getNomeCompleto() {
+    return `Cliente ${this.nome} ${this.sobrenome}`;
+  }
+}
+
+const aluno = new Aluno('Marcus', 'Boni', 19, 23784);
+const cliente = new Cliente('Marcus', 'Boni', 19, 23784);
+
+console.log(aluno.getNomeCompleto());
+console.log(cliente.getNomeCompleto());
+
+export class Udemy extends Empresa {
+  constructor() {
+    super('Udemy', '11.111.111/0001-11');
+  }
+
+  popColaborador(): Colaborador | null {
+    const colaborador = this.colaboradores.pop();
+    if (colaborador) return colaborador;
+    return null;
+  }
+}
+
+const empresa2 = new Udemy();
+
+console.log(empresa2);
+
+export class Someone {
+  static idadePadrao = 10;
+  static cpfPadrao = '999-111-222.23';
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    public idade: number,
+    public _cpf: string,
+  ) {}
+
+  static criaPessoa(nome: string, sobrenome: string): Someone {
+    return new Someone(nome, sobrenome, Someone.idadePadrao, Someone.cpfPadrao);
+  }
+
+  get cpf(): string {
+    return this._cpf;
+  }
+
+  set cpf(cpf: string) {
+    this._cpf = cpf;
+  }
+}
+
+const someone = new Someone('Marcus', 'Boni', 20, '222-222-333.22');
+someone.cpf = '111';
+
+console.log(someone);
